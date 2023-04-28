734925a6a9cf88b4e42f79faa1837006579211f8
Mayvois <mamoadad@yahoo.com>
2023-04-28 15:50:24 -0400

    load_libraries

---
+++ main.R
@@ -0,0 +1,11 @@
+# Load packages
+library(tidyr)
+library(dplyr)
+library(naniar)
+library(ggplot2)
+library(CGPfunctions)
+require(graphics) 
+library(cowplot) # used for correlation
+library(car) # used for VIF
+library(scatterplot3d) #used for graph
+library(corrplot) # used for correlation
