2b9c56888f144399af059abbaeb34612152a01e0
Nahid Aghababaeyan <nahid.aut@gmail.com>
2023-05-02 15:16:00 -0400

    load the dataset and explaination of columns

--- main.R
+++ main.R
@@ -31,3 +31,26 @@ PlotXTabs(da, sex, target)
 
 
 
+########### Nahid
+# Read in a CSV file named "heart.csv" located in the "data" directory relative to the current working directory
+dataset <- read.csv("./data/heart.csv")
+
+# Display the first few rows of the dataset to check that it was read in correctly
+head(dataset)
+
+#About columns
+"""
+heart.csv dataset has the following columns:
+age: the age of the patient (in years)
+sex: the patient's sex (1 = male, 0 = female)
+cp: the type of chest pain experienced by the patient (1 = typical angina, 2 = atypical angina, 3 = non-anginal pain, 4 = asymptomatic) trestbps: the patient's resting blood pressure (in mm Hg on admission to the hospital)
+chol: the patient's cholesterol measurement (in mg/dl)
+fbs: the patient's fasting blood sugar (> 120 mg/dl or not; 1 = true, 0 = false)
+restecg: resting electrocardiographic results (0 = normal, 1 = having ST-T wave abnormality, 2 = showing probable or definite left ventricular hypertrophy)
+thalach: the patient's maximum heart rate achieved during exercise
+exang: whether the patient experienced exercise-induced angina (1 = yes, 0 = no)
+oldpeak: ST depression induced by exercise relative to rest
+slope: the slope of the peak exercise ST segment (1 = upsloping, 2 = flat, 3 = downsloping)
+ca: the number of major vessels (0-3) colored by fluoroscopy
+thal: a categorical variable indicating the patient's thalassemia status (3 = normal, 6 = fixed defect, 7 = reversable defect)
+"""
