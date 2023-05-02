a972f88481df0a42b47a901bf25959c8a0b55d91
Nahid Aghababaeyan <nahid.aut@gmail.com>
2023-05-02 18:32:35 -0400

    Deal with out liers

--- main.R
+++ main.R
@@ -33,7 +33,7 @@ PlotXTabs(da, sex, target)
 
 ########### Nahid
 
-#Reading libraries
+#1[Reading libraries]
 library(caret)
 library(naniar)
 library(mltools)
@@ -48,6 +48,7 @@ library(plyr)
 options(warn=-1)
 options(repr.plot.width=20, repr.plot.height = 12)
 
+#2[Load and Explain dataset]
 
 # Read in a CSV file named "heart.csv" located in the "data" directory relative to the current working directory
 dataset <- read.csv("./data/heart.csv")
@@ -71,6 +72,8 @@ slope: the slope of the peak exercise ST segment (1 = upsloping, 2 = flat, 3 = d
 ca: the number of major vessels (0-3) colored by fluoroscopy
 thal: a categorical variable indicating the patient's thalassemia status (3 = normal, 6 = fixed defect, 7 = reversable defect)
 """
+#3[Deal with missing values]
+
 # Check for missing values
 vis_miss(dataset) + 
   ggtitle("Missing values for each variable")+
@@ -79,6 +82,8 @@ vis_miss(dataset) +
 
 # The plot showed that there were no missing values.
 # It seems like a data cleaning and preprocessing step was taken to ensure the quality and integrity of the data before further analysis was done.
+
+#4[Data Cleaning]
 
 #Lableing data
 dataset <- distinct(dataset)
@@ -93,7 +98,7 @@ dataset$target <- factor(dataset$target)
 df<-dataset[dataset$ca!="4" & dataset$thal !="0",]
 df$thal <- factor(dataset2$thal, labels = c("normal","fixed defect","reversable defect"))
 
-#show the cleaned data
+#5[show the cleaned data]
 
 # Use the 'glimpse' function from the 'dplyr' package to display a concise summary of the 'df' dataframe
 glimpse(df)
@@ -103,6 +108,8 @@ head(df)
 summary(df)
 # Number of observations reduced to 296 from 303
 # 9 variables converted to factors, 5 remain numeric
+
+#6 [Check the importance of variables]
 
 # Although it is evident that certain variables will be crucial for the model, it is still necessary to evaluate the importance of each variable using statistical tests. 
 # This will help to ensure that only the most relevant variables are included in the model, 
@@ -114,21 +121,41 @@ library(rstatix)
 # create a data frame with v-cramer factor values for categorical variables
 data.frame(value = apply(df[,c(2,3,6,7,9,11,12,13)],2, FUN = function(x) {cramer.v(table(df$target,x))})) %>%
   
-  # plot the v-cramer values using ggplot2
-  ggplot(aes(x = colnames(df[,c(2,3,6,7,9,11,12,13)]), y=value))+
-  
-  # create a bar plot of v-cramer values
-  geom_col( fill = "green", color ="grey", alpha = 0.6, width = 0.7)+
-  
-  # add plot title and axis labels
-  labs(title ="V-Cramer plot", y = "Value", x ="Variables")+
-  
-  # customize the y-axis scale
-  scale_y_continuous(breaks = seq(0,0.6,0.05))+
-  
-  # customize plot theme
-  theme(plot.background = element_rect(fill = "#fff8ab"), panel.background = element_rect(fill = "#fff8ab"), 
-        legend.background = element_rect(fill = "#fff8ab"), 
-        axis.line = element_line(colour = "#636360"), plot.title = element_text(size =30,hjust =0.5), plot.title.position = "plot", panel.grid.major.x = element_blank(),
-        panel.grid = element_line(colour = "grey"), axis.title.y = element_text(size =28), axis.text = element_text(size =25),
-        axis.title.x = element_text(size =28))
+# plot the v-cramer values using ggplot2
+ggplot(aes(x = colnames(df[,c(2,3,6,7,9,11,12,13)]), y=value))+
+
+# create a bar plot of v-cramer values
+geom_col( fill = "green", color ="grey", alpha = 0.6, width = 0.7)+
+
+# add plot title and axis labels
+labs(title ="V-Cramer plot", y = "Value", x ="Variables")+
+
+# customize the y-axis scale
+scale_y_continuous(breaks = seq(0,0.6,0.05))+
+
+# customize plot theme
+theme(plot.background = element_rect(fill = "#fff8ab"), panel.background = element_rect(fill = "#fff8ab"), 
+    legend.background = element_rect(fill = "#fff8ab"), 
+    axis.line = element_line(colour = "#636360"), plot.title = element_text(size =30,hjust =0.5), plot.title.position = "plot", panel.grid.major.x = element_blank(),
+    panel.grid = element_line(colour = "grey"), axis.title.y = element_text(size =28), axis.text = element_text(size =25),
+    axis.title.x = element_text(size =28))
+
+#7 [Deal with out liers]
+
+outliers <- function(x)
+{
+  d <- which(x<=(mean(x)-3*sd(x))) # down border
+  u <- which(x>=(mean(x)+3*sd(x))) # up border
+  if(length(d)>0) # replacing bottom observations if needed
+  {
+    m_d <- min(x[-d])
+    x[d]<- m_d
+  }
+  if(length(u)>0) # replacing upper observations if needed
+  {
+    m_u <- max(x[-u])
+    x[u]<-m_u
+  }
+  return(x) 
+}
+df[,c(1,4,5,8,10)] <- apply(df[,c(1,4,5,8,10)], 2, outliers)
